0115ed4fc9bb01d56577fe2d56584c911bae3d67
Alexander Ryazanov <alryaz@xavux.com>
2021-04-14 23:16:26 +0300

    Testing changes

--- proxy_1/data/run.sh
+++ proxy_1/data/run.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bashio
 set -e
 
-DESTINATION=$(bashio::config 'destination')
-DEFAULT_CONF=$(bashio::config 'default_conf')
-ADDITIONAL_CONF=$(bashio::config 'additional_conf')
-
 if bashio::config.true 'cloudflare'; then
     sed -i "s|#include /data/cloudflare.conf;|include /data/cloudflare.conf;|" /etc/nginx.conf
     # Generate cloudflare.conf
@@ -29,27 +25,27 @@ if bashio::config.true 'cloudflare'; then
     fi
 fi
 
+# Prepare substitutions
 ingress_entry="$(bashio::addon.ingress_entry)"
 ingress_entry_escaped="${ingress_entry//\//\\/}"
 
-if [ -n "${DEFAULT_CONF}" ]; then
-    cp "/share/${DEFAULT_CONF}" "/tmp/default.conf" \
-    && sed -i "s|%%base_path%%|${ingress_entry}|g" /tmp/default.conf \
-    && sed -i "s|%%base_path_escaped%%|${ingress_entry_escaped}|g" /tmp/default.conf \
-    && sed -i "s|#include /tmp/default.conf;|include /tmp/default.conf;|" /etc/nginx.conf \
-    || exit 1
-fi
+for conf_id in default additional external; do
+    conf_file=$(bashio::config $conf_id'_conf')
+    if [ -n "${conf_file}" ]; then
+        conf_target="/tmp/${conf_id}"
 
-if [ -n "${ADDITIONAL_CONF}" ]; then
-    cp "/share/${ADDITIONAL_CONF}" "/tmp/additional.conf" \
-        && sed -i "s|%%base_path%%|${ingress_entry}|g" /tmp/additional.conf \
-        && sed -i "s|%%base_path_escaped%%|${ingress_entry_escaped}|g" /tmp/default.conf \
-        && sed -i "s|#include /tmp/additional.conf;|include /tmp/additional.conf;|" /etc/nginx.conf \
+        bashio::log.info "Copying \"${conf_file}\" to \"${conf_target}\"..."
+
+        cp "/share/${conf_file}" "${conf_target}" \
+        && sed -i "s|%%base_path%%|${ingress_entry}|g" $conf_target \
+        && sed -i "s|%%base_path_escaped%%|${ingress_entry_escaped}|g" $conf_target \
+        && sed -i "s|#include ${conf_target};|include ${conf_target};|" /etc/nginx.conf \
         || exit 1
-fi
+    fi
+done
 
 # Prepare config file
-sed -i "s#%%DESTINATION%%#$DESTINATION#g" /etc/nginx.conf
+sed -i "s#%%DESTINATION%%#$(bashio::config 'destination')#g" /etc/nginx.conf
 
 bashio::log.info "Running nginx..."
 exec nginx -c /etc/nginx.conf < /dev/null
